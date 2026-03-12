ab8a448e7d94bc46dcae4138a0d6c0fa5aa21c6f
Aucky Riman Halim <auckyrh@gmail.com>
2026-03-13 00:14:39 +0700

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,17 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('users')->insert([
+            'full_name' => 'Super Admin',
+            'email'     => 'superadmin@santoyakobus.org',
+            'password'  => Hash::make('zxcvzxcv'),
+            'role'      => 'superadmin',
+        ]);
 
         $this->call([
             WilayahLingkunganSeeder::class,
             DivisionSeeder::class,
+            GatheringTypeSeeder::class,
+            EventPeriodSeeder::class,
         ]);
 
         // User::factory(10)->create();
-
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
     }
 }
