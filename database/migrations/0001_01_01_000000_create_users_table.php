563e4e3a6964309308bce7344419293b2e56521a
Aucky Riman Halim <auckyrh@gmail.com>
2026-03-13 00:12:56 +0700

    remove unused migration files, combine into 1 create users file.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('nick_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->enum('role', ['superadmin', 'admin', 'member'])->default('member');
+            $table->string('photo')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
