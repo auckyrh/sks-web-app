5503b9318130c7065c4b7a719b4f6a6f3ebaddf8
Aucky Riman Halim <auckyrh@gmail.com>
2026-03-12 20:44:03 +0700

    show data on Wilayah & Lingkungan Filament Resource

--- app/Filament/Resources/LingkunganResource.php
+++ app/Filament/Resources/LingkunganResource.php
@@ -16,28 +16,54 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class LingkunganResource extends Resource
 {
     protected static ?string $model = Lingkungan::class;
+    protected static ?string $navigationGroup = 'Master Data';
+    protected static ?string $navigationLabel = 'Lingkungan';
+    protected static ?int $navigationSort = 2;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                //
-            ]);
+        return $form->schema([
+            Forms\Components\Select::make('wilayah_id')
+                ->label('Wilayah')
+                ->relationship('wilayah', 'name')
+                ->searchable()
+                ->preload()
+                ->required(),
+            Forms\Components\TextInput::make('name')
+                ->label('Nama Lingkungan')
+                ->required()
+                ->maxLength(255),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('wilayah.name')
+                    ->label('Wilayah')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama Lingkungan')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime('d M Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Tables\Filters\SelectFilter::make('wilayah')
+                    ->relationship('wilayah', 'name')
+                    ->label('Filter Wilayah'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/WilayahResource.php
+++ app/Filament/Resources/WilayahResource.php
@@ -16,28 +16,44 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class WilayahResource extends Resource
 {
     protected static ?string $model = Wilayah::class;
+    protected static ?string $navigationGroup = 'Master Data';
+    protected static ?string $navigationLabel = 'Wilayah';
+    protected static ?int $navigationSort = 1;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                //
-            ]);
+        return $form->schema([
+            Forms\Components\TextInput::make('name')
+                ->label('Nama Wilayah')
+                ->required()
+                ->maxLength(255),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama Wilayah')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('lingkungan_count')
+                    ->label('Jumlah Lingkungan')
+                    ->counts('lingkungan')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime('d M Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
-            ->filters([
-                //
-            ])
+            ->filters([])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
