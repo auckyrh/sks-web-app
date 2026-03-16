65b58b6dab075c866f1f1ae6d498d5c0a8d27456
Aucky Riman Halim <auckyrh@gmail.com>
2026-03-17 01:54:31 +0700

    reorder migrations. add event logo on seeder

--- database/seeders/EventPeriodSeeder.php
+++ database/seeders/EventPeriodSeeder.php
@@ -26,26 +26,28 @@ class EventPeriodSeeder extends Seeder
             ['year' => 2020, 'theme' => 'Jesus is My Saviour'],
 
             // Specific records
-            ['year' => 2021, 'theme' => 'Back to My Saviour',     'start' => '2021-06-24', 'end' => '2021-06-26'],
-            ['year' => 2022, 'theme' => 'Unite with Jesus',       'start' => '2022-06-28', 'end' => '2022-06-30'],
-            ['year' => 2023, 'theme' => 'Jesus is My Anchor',     'start' => '2023-06-20', 'end' => '2023-06-22'],
+            ['year' => 2021, 'theme' => 'Back to My Saviour', 'start' => '2021-06-24', 'end' => '2021-06-26'],
+            ['year' => 2022, 'theme' => 'Unite with Jesus',   'start' => '2022-06-28', 'end' => '2022-06-30'],
+            ['year' => 2023, 'theme' => 'Jesus is My Anchor', 'start' => '2023-06-20', 'end' => '2023-06-22', 'event_logo' => 'event-logos/LOGO-SKS-2023.jpeg'],
             ['year' => 2024, 'theme' => '-'],
-            ['year' => 2025, 'theme' => 'Messenger of Joy & Love','start' => '2025-06-24', 'end' => '2025-06-26'],
-            ['year' => 2026, 'theme' => 'Yeeess Lord','start' => '2026-06-24', 'end' => '2026-06-26'],
+            ['year' => 2025, 'theme' => 'Messenger of Joy & Love','start' => '2025-06-24', 'end' => '2025-06-26' , 'event_logo' => 'event-logos/LOGO-SKS-2026.png'],
+            ['year' => 2026, 'theme' => 'Yeeess Lord','start' => '2026-06-24', 'end' => '2026-06-26', 'event_logo' => 'event-logos/LOGO-SKS-2025.png'],
         ];
 
         $count = 0;
 
         foreach ($periods as $data) {
-            $year      = $data['year'];
-            $startDate = $data['start'] ?? "{$year}-06-26";
-            $endDate   = $data['end']   ?? "{$year}-06-28";
-            $isActive  = $year === 2026;
+            $year       = $data['year'];
+            $event_logo = $data['event_logo'] ?? null;
+            $startDate  = $data['start'] ?? "{$year}-06-26";
+            $endDate    = $data['end']   ?? "{$year}-06-28";
+            $isActive   = $year === 2026;
 
             EventPeriod::firstOrCreate(
                 ['year' => $year],
                 [
                     'theme'                 => $data['theme'],
+                    'event_logo'            => $event_logo,
                     'is_active'             => $isActive,
                     'event_start_date'      => $startDate,
                     'event_end_date'        => $endDate,
